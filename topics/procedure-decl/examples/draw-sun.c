8a689c695aad77cf3d0ee1e7091e6f4d0719433d
Ray Guo <ma2808203259@hotmail.com>
2022-04-08 15:27:31 +1000

    refactor: convert button_click,draw sun and etc.

--- topics/procedure-decl/examples/draw-sun.c
+++ topics/procedure-decl/examples/draw-sun.c
@@ -1,36 +1,34 @@
 /* Program: Draw Sun Scene */
-#include "SwinGame.h"
+#include "splashkit.h"
 
 void draw_sun()
 {
-    //Draw the sun
-    fill_circle(ColorYellow, 50.0, 50.0, 20);
-    draw_circle(ColorRed, 50.0, 50.0, 15);
-    draw_circle(ColorRed, 50.0, 50.0, 12);
-    draw_circle(ColorRed, 50.0, 50.0, 9);
-    draw_circle(ColorRed, 50.0, 50.0, 6);
-    fill_circle(ColorWhite, 50.0, 50.0, 3);
-    draw_line(ColorYellow, 50, 75, 50, 110);
-    draw_line(ColorYellow, 25, 75, 0, 100);
-    draw_line(ColorYellow, 75, 75, 100, 100);
-    draw_line(ColorYellow, 0, 50, 25, 50);
-    draw_line(ColorYellow, 75, 50, 100, 50);
-    draw_line(ColorYellow, 25, 25, 0, 0);
-    draw_line(ColorYellow, 75, 25, 100, 0);
-    draw_line(ColorYellow, 50, 0, 50, 25);
+    // Draw the sun
+    fill_circle(color_yellow(), 50.0, 50.0, 20);
+    draw_circle(color_red(), 50.0, 50.0, 15);
+    draw_circle(color_red(), 50.0, 50.0, 12);
+    draw_circle(color_red(), 50.0, 50.0, 9);
+    draw_circle(color_red(), 50.0, 50.0, 6);
+    fill_circle(color_white(), 50.0, 50.0, 3);
+    draw_line(color_yellow(), 50, 75, 50, 110);
+    draw_line(color_yellow(), 25, 75, 0, 100);
+    draw_line(color_yellow(), 75, 75, 100, 100);
+    draw_line(color_yellow(), 0, 50, 25, 50);
+    draw_line(color_yellow(), 75, 50, 100, 50);
+    draw_line(color_yellow(), 25, 25, 0, 0);
+    draw_line(color_yellow(), 75, 25, 100, 0);
+    draw_line(color_yellow(), 50, 0, 50, 25);
 }
 
 int main()
 {
-    open_graphics_window("Draw Sun", 800, 600);
-    load_default_colors();
-    
-    clear_screen_to(ColorBlue);
+    open_window("Draw Sun", 800, 600);
+
+    clear_screen(color_blue());
     draw_sun();
-    
+
     refresh_screen();
     delay(5000);
-    
-    release_all_resources();
+
     return 0;
 }
